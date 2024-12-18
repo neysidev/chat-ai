bba8f95220cfea9f36d9e2830bc1b46d7cec0e6f
Mety <dev.mehdineysi@gmail.com>
2024-12-19 02:33:21 +0330

    feat: initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import clsx from "clsx"
+import { Open_Sans } from "next/font/google"
+import { FC, PropsWithChildren } from "react"
+
+import { siteMetadata } from "@/data/siteMetaData"
+import "@/styles/globals.css"
+
+const openSans = Open_Sans({ variable: "--font-open-sans", subsets: ["latin"] })
+export const metadata = siteMetadata
+
+const RootLayout: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <html lang="en">
+      <body className={clsx(openSans.variable, "antialiased")}>{children}</body>
+    </html>
+  )
+}
+
+export default RootLayout
---
+++ data/siteMetaData.ts
@@ -0,0 +1,6 @@
+import type { Metadata } from "next"
+
+export const siteMetadata: Metadata = {
+  title: "Chat AI",
+  description: "Minimal, elegant, and effortless AI chat experience.",
+}
