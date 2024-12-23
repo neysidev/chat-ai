0e4404f690963377a26bdd809a8aa520d725608e
Mety <dev.mehdineysi@gmail.com>
2024-12-23 22:48:29 +0330

    feat(explore): make header responsive

--- app/explore/_components/Header.tsx
+++ app/explore/_components/Header.tsx
@@ -1,8 +1,13 @@
 export default function Header() {
   return (
-    <header data-title="Header" className="space-y-2 text-center w-2/3 mx-auto">
-      <h1 className="text-4xl font-semibold">Explore</h1>
-      <p className="text-neutral-600">
+    <header
+      data-title="Header"
+      className="transition-all space-y-2 text-center sm:w-4/5 md:w-2/3 mx-auto"
+    >
+      <h1 className="transition-all text-3xl md:text-4xl font-semibold">
+        Explore
+      </h1>
+      <p className="transition-all text-sm md:text-base text-neutral-600">
         Discover and create custom versions of ChatGPT that combine
         instructions, extra knowledge, and any combination of skills.
       </p>
