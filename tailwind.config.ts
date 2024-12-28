25ee40baef3584fcd43f64534d2ce326a52eaa9b
Mety <dev.mehdineysi@gmail.com>
2024-12-29 02:32:14 +0330

    feat: add shine animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,9 +9,14 @@ export default {
   theme: {
     extend: {
       animation: {
+        shine: "shine 5s infinite linear",
         rotate360: "rotate360 0.75s linear infinite",
       },
       keyframes: {
+        shine: {
+          "0%": { backgroundPosition: "-200px center" },
+          "100%": { backgroundPosition: "200px center" },
+        },
         rotate360: {
           "0%": { transform: "rotate(0deg)" },
           "100%": { transform: "rotate(360deg)" },
