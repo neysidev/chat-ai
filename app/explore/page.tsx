2dd260133e030c6125d1c8a0b06e1f45d96918e6
Mety <dev.mehdineysi@gmail.com>
2024-12-28 20:26:23 +0330

    feat: add title template

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,6 +1,11 @@
+import type { Metadata } from "next"
 import Categories from "./_components/Categories"
 import Header from "./_components/Header"
 import Search from "./_components/Search"
+
+export const metadata: Metadata = {
+  title: "Explore",
+}
 
 export default function Explore() {
   return (
--- data/siteMetaData.ts
+++ data/siteMetaData.ts
@@ -1,6 +1,9 @@
 import type { Metadata } from "next"
 
 export const siteMetadata: Metadata = {
-  title: "Chat AI",
+  title: {
+    template: "%s | Chat AI",
+    default: "Chat AI",
+  },
   description: "Minimal, elegant, and effortless AI chat experience.",
 }
